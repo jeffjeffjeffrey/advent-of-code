b66cca21dd8574ed592154b6c6fe97a35f4813f8
jeffjeffjeffrey <jeffrey@kickstarter.com>
2018-12-11 09:20:08 -0500

    day 11

---
+++ 2018-python/day11.py
@@ -0,0 +1,59 @@
+grid_size = 300
+grid_serial_number = int(input())
+
+# Part 1
+
+def get_power(x, y):
+  return int(((x + 10) * y + grid_serial_number) * (x + 10) / 100) % 10 - 5
+
+# 300x300 grid with values equal to each square's power
+grid = [[0] * grid_size for i in range(grid_size)]
+
+# Start at the bottom-right and iterate through the grid,
+# computing the single-square power along with the 3x3 subsquare power along the way
+optimal_subsquare = {'point': None, 'power': None}
+for i in range(grid_size):
+  for j in range(grid_size):
+    x = grid_size - i - 1
+    y = grid_size - j - 1
+    grid[x][y] = get_power(x, y)
+    if x + 3 <= grid_size and y + 3 <= grid_size:
+      subsquare_power = 0
+      for a in range(3):
+        for b in range(3):
+          subsquare_power += grid[x + a][y + b]
+      if not optimal_subsquare['power'] or optimal_subsquare['power'] < subsquare_power:
+        optimal_subsquare['point'] = (x, y)
+        optimal_subsquare['power'] = subsquare_power
+
+print(optimal_subsquare)
+
+# Part 2
+
+# 300x300 grid of dictionaries: top-left coordinates -> subsquare size -> total power
+grid = [[{0:0, -1:0} for j in range(grid_size)] for i in range(grid_size)]
+
+# Start at the bottom-right and iterate through the grid,
+# computing the all subsquare powers along the way
+# using inclusion-exclusion on previously-computed subsquares
+optimal_subsquare = {'point': None, 'size': None, 'power': None}
+for i in range(grid_size):
+  for j in range(grid_size):
+    x = grid_size - i - 1
+    y = grid_size - j - 1
+    for square_size in range(1, min(grid_size - x, grid_size - y) + 1):
+      if square_size == 1:
+        grid[x][y][square_size] = get_power(x, y)
+      else:
+        grid[x][y][square_size] = \
+          grid[x][y][1] + \
+          grid[x + 1][y][square_size - 1] + \
+          grid[x][y + 1][square_size - 1] - \
+          grid[x + 1][y + 1][square_size - 2] + \
+          grid[x + square_size - 1][y + square_size - 1][1]
+      if not optimal_subsquare['power'] or optimal_subsquare['power'] < grid[x][y][square_size]:
+        optimal_subsquare['point'] = (x, y)
+        optimal_subsquare['size'] = square_size
+        optimal_subsquare['power'] = grid[x][y][square_size]
+
+print(optimal_subsquare)
