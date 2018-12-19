ff253eb65e32d00a6fef21b5136be906caaa0b8f
jeffjeffjeffrey <jeffrey@kickstarter.com>
2018-12-19 18:11:35 -0500

    clean up comments

--- 2018-python/day19.py
+++ 2018-python/day19.py
@@ -127,7 +127,7 @@ def run_program(ip, register, verbose = False):
       instruction['C']
     )
     if verbose:
-      print(ip, register, instruction['opcode'],[instruction['A'], instruction['B'], instruction['C']], next_register)
+      print("ip:", ip, register, instruction['opcode'],[instruction['A'], instruction['B'], instruction['C']], next_register)
     register = next_register
     ip = register[ip_index] + 1
   return register
@@ -145,39 +145,39 @@ print("Part 1 program output:", register[0])
 # Same as part 1, but begin with register [1, 0, 0, 0, 0, 0]
 
 # The program runs for a long time withouth halting. Something is up.
-# After breaking execution and analyzing its registers at several different points, we have an epiphany:
-# It is calculating the sum of all the factors of a very big number N.
+# After breaking execution and analyzing the registers at several different points,
+# we start to get a sense of what this program is doing:
 
-# For Part 1, N = 875
-# For Part 2, N = 10551275 (yikes!)
+# The program starts by multiplying r0 by a bunch of stuff to get a big number N.
+# Then we enter some loops:
 
-# Here is what the 6 registers are doing:
-
-# r4 holds N
-# r0 holds the running sum of factors
+# r4 holds onto the big number N
 # r1 and r3 each iterate from 1 to N
-# if r1 * r3 == N, then add the factor r1 to r0
+# if r1 * r3 == N, then add r1 to r0
 
 # r5 is used for logical flow
 # r2 is the instruction pointer
 
-# The program starts by multiplying r0 by a bunch of stuff to get a big number N.
-# If the rest of the program were written in Python, it might look like this:
+# Rewriting this logic in Python looks like this:
 
-# factor_sum = 0
+# sum = 0
 # for i in range(1, N+1):
 #   for j in range(1, N+1):
 #     if i * j == N:
-#       factor_sum += i
+#       sum += i
 
-# We can bypass this very inefficient loop by computing the factor sum using math.
+# Eureka! This is simply finding the sum of the divisors of N.
+# For Part 1, N = 185. We could use brute force.
+# For Part 2, N = 10551275. Yikes! Brute force would never work here.
+
+# Luckily, we can bypass this very inefficient loop by computing the divisor sum using math.
 
 # To see the end of the program, begin here:
 print("The final few iterations of Part 2:")
 N = 10551275
-factor_sum = sum([1, 5, 7, 25, 35, 175, 60293, 301465, 422051, 1507325, 2110255, 10551275])
+divisor_sum = sum([1, 5, 7, 25, 35, 175, 60293, 301465, 422051, 1507325, 2110255, 10551275])
 ip = 9
-register = [factor_sum, N, 9, N, N, 0]
+register = [divisor_sum, N, 9, N, N, 0]
 
 register = run_program(ip, register, True)
 print("Part 2 program output:", register[0])
